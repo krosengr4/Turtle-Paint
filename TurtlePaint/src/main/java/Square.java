784acf21e0683ea08a8cb74f3cf1cda8d9f6bb54
krosengr4 <rosenkev4@gmail.com>
2025-05-21 04:11:56 -0500

    Created Square class that is a child of Shape. Included Constructtor, geters and settters, and overrided paint method

--- TurtlePaint/src/main/java/Square.java
+++ TurtlePaint/src/main/java/Square.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.awt.*;
 
-public class Square {
+public class Square extends Shape {
+
+    int sideLength;
+
+    public Square(Turtle turtle, Point location, String color, int border, int sideLength) {
+        super(turtle, location, color, border);
+        this.sideLength = sideLength;
+    }
+
+    //region getters and setters
+    public void setSideLength(int sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    public int getSideLength() {
+        return sideLength;
+    }
+    //endregion
+
+    @Override
+    public void paint() {
+        System.out.println("Paint Square!");
+    }
 }
