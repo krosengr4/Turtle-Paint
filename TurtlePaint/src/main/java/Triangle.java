531ff36e6ce0fa89d115f129b4ca1741d75a4138
krosengr4 <rosenkev4@gmail.com>
2025-05-21 04:36:47 -0500

    Created Triangle Class that is a child of shape with constructor, getters, setters, and paint method

---
+++ TurtlePaint/src/main/java/Triangle.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+
+public class Triangle extends Shape{
+
+    int base;
+    int height;
+
+    public Triangle(Turtle turtle, Point location, String color, int border, int base, int height) {
+        super(turtle, location, color, border);
+        this.base = base;
+        this.height = height;
+    }
+
+    //region Getters and Setters
+    public int getBase() {
+        return base;
+    }
+
+    public void setBase(int base) {
+        this.base = base;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    //endregion
+
+    @Override
+    public void paint() {
+        System.out.println("Paint the triangle!");
+    }
+}
