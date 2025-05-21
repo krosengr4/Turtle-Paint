ff46eaecaa8e2cf446ef9436f710c6d017f83c6f
krosengr4 <rosenkev4@gmail.com>
2025-05-21 04:39:46 -0500

    Added Hexagon class that is a child of shape with constructor, getters, setters, and paint method

---
+++ TurtlePaint/src/main/java/Hexagon.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+
+public class Hexagon extends Shape{
+
+    int sideLength;
+
+    public Hexagon (Turtle turtle, Point location, String color, int border, int sideLength) {
+        super(turtle, location, color, border);
+        this.sideLength = sideLength;
+    }
+
+    //region getters and setters
+    public int getSideLength() {
+        return sideLength;
+    }
+
+    public void setSideLength(int sideLength) {
+        this.sideLength = sideLength;
+    }
+    //endregion
+
+    public void paint() {
+        System.out.println("Paint the hexagon!");
+    }
+}
