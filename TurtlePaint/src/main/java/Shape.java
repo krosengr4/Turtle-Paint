95ab491b430016eb6bbc1ba21605e78699c89a4f
krosengr4 <rosenkev4@gmail.com>
2025-05-21 04:11:08 -0500

    Created constructor for Shape class

--- TurtlePaint/src/main/java/Shape.java
+++ TurtlePaint/src/main/java/Shape.java
@@ -7,6 +7,12 @@ public abstract class Shape {
     String color; //<--- color used for border
     int border; //<--- width of the shape border
 
+    public Shape (Turtle turtle, Point location, String color, int border) {
+        this.turtle = turtle;
+        this.location = location;
+        this.color = color;
+        this.border = border;
+    }
 
     public abstract void paint();
 
