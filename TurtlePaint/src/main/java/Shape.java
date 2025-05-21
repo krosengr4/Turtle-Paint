5d2d7403a00d718c3be7002c343f668a9987dba5
krosengr4 <rosenkev4@gmail.com>
2025-05-21 04:13:11 -0500

    Created getters and setters for Shape class

--- TurtlePaint/src/main/java/Shape.java
+++ TurtlePaint/src/main/java/Shape.java
@@ -14,6 +14,40 @@ public abstract class Shape {
         this.border = border;
     }
 
+    //region getter and setters
+    public Turtle getTurtle() {
+        return turtle;
+    }
+
+    public void setTurtle(Turtle turtle) {
+        this.turtle = turtle;
+    }
+
+    public Point getLocation() {
+        return location;
+    }
+
+    public void setLocation(Point location) {
+        this.location = location;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getBorder() {
+        return border;
+    }
+
+    public void setBorder(int border) {
+        this.border = border;
+    }
+    //endregion
+
     public abstract void paint();
 
 }
